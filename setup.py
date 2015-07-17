712d21e815c06b5ef4d5a332428ef8229ee4c331
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-07-17 12:55:14 +0200

    Let's be pep8 valid in the setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 setup(
-    name = 'libpagure',
-    packages = ['libpagure'],
-    version = '0.21',
-    description = 'A Python library for Pagure APIs.',
-    author = 'Lei Yang',
-    author_email = 'yltt1234512@gmail.com',
-    url = 'https://github.com/yangl1996/libpagure',
-    download_url = 'https://github.com/yangl1996/libpagure/tarball/0.21',
-    keywords = ['pagure', 'api', 'library'],
-    classifiers = [
+    name='libpagure',
+    packages=['libpagure'],
+    version='0.21',
+    description='A Python library for Pagure APIs.',
+    author='Lei Yang',
+    author_email='yltt1234512@gmail.com',
+    url='https://github.com/yangl1996/libpagure',
+    download_url='https://github.com/yangl1996/libpagure/tarball/0.21',
+    keywords=['pagure', 'api', 'library'],
+    classifiers=[
         'Programming Language :: Python',
     ],
-    license = "GNU General Public License v2.0",
+    license='GNU General Public License v2.0',
 )
