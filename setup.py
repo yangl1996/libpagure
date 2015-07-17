6c4e9075e2f62c949b27c57c993b1b232a554e28
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-07-17 12:54:07 +0200

    Remove comments in the setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup
 setup(
     name = 'libpagure',
-    packages = ['libpagure'], # this must be the same as the name above
+    packages = ['libpagure'],
     version = '0.21',
     description = 'A Python library for Pagure APIs.',
     author = 'Lei Yang',
     author_email = 'yltt1234512@gmail.com',
-    url = 'https://github.com/yangl1996/libpagure', # use the URL to the github repo
-    download_url = 'https://github.com/yangl1996/libpagure/tarball/0.21', # I'll explain this in a second
-    keywords = ['pagure', 'api', 'library'], # arbitrary keywords
-    classifiers = ['Programming Language :: Python'],
-    license = "GNU General Public License v2.0"
+    url = 'https://github.com/yangl1996/libpagure',
+    download_url = 'https://github.com/yangl1996/libpagure/tarball/0.21',
+    keywords = ['pagure', 'api', 'library'],
+    classifiers = [
+        'Programming Language :: Python',
+    ],
+    license = "GNU General Public License v2.0",
 )
