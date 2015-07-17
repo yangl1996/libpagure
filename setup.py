b342cba1887827916317f43782888b863f9f8d7f
Lei Yang <yltt1234512@gmail.com>
2015-07-17 20:55:26 +0800

    modify setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 setup(
     name='libpagure',
     packages=['libpagure'],
-    version='0.21',
+    version='0.3',
     description='A Python library for Pagure APIs.',
     author='Lei Yang',
     author_email='yltt1234512@gmail.com',
     url='https://github.com/yangl1996/libpagure',
-    download_url='https://github.com/yangl1996/libpagure/tarball/0.21',
     keywords=['pagure', 'api', 'library'],
     classifiers=[
         'Programming Language :: Python',
