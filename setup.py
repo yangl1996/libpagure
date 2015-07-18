29e41ddce834806049337d60d04643c98c7743cf
Pierre-Yves Chibon <pingou@pingoured.fr>
2015-07-18 13:12:14 +0200

    Expand the list of topics for libpagure
    Picked from the list available at:
    https://pypi.python.org/pypi?%3Aaction=list_classifiers

--- setup.py
+++ setup.py
@@ -13,7 +13,18 @@ setup(
     url='https://github.com/yangl1996/libpagure',
     keywords=['pagure', 'api', 'library'],
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v2 '
+        'or later (GPLv2+)',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+
     ],
     license='GNU General Public License v2.0',
     install_requires=['requests'],
